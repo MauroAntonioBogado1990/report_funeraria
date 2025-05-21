c37d1f3ae57f02fd3f742ae44758060427240c41
MauroAntonioBogado1990 <mauroantoniobogado@gmail.com>
2025-05-21 17:16:40 -0300

    add report in sale for Odoo 16 for determinated printer

---
+++ exe_reporte_funeraria_murziez/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+
+{
+    'name': 'Exe reporte de Funeraria',
+    'version': '16.0',
+    'depends': [
+        'base','sale','stock','product', 'account',
+    ],
+    'author': 'Mauro Bogado, Exemax',
+    'website': '"http://www.exemax.com.ar"',
+    'summary': 'Modulo para reporte de Funeraria para impresora',
+    'category': 'Extra Tools',
+    'description': '''
+   'Modulo para remitos'.
+    ''',
+    'data': [
+       'report/reporte_funeraria.xml',
+        
+       
+    ],
+}
